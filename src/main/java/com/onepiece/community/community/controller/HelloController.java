6dfc1e77b8686ae7459f7353b6cc311e1e68b5f7
unknown <1510147411@qq.com>
2021-03-02 17:32:47 +0800

    init rep

---
+++ src/main/java/com/onepiece/community/community/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.onepiece.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller //自动识别扫描当前的类，把它作为spring的bean去管理，同时视其为一个controller（可以接收前端的请求）
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name,Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
