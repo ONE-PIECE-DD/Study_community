89ee0293ef4393feab06bc8f378bb197c5131c3a
ONE-PIECE-D <1510147411@qq.com>
2021-03-03 16:03:43 +0800

    change html

--- src/main/java/com/onepiece/community/community/controller/IndexController.java
+++ src/main/java/com/onepiece/community/community/controller/IndexController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller //自动识别扫描当前的类，把它作为spring的bean去管理，同时视其为一个controller（可以接收前端的请求）
 public class HelloController {
-    @GetMapping("/hello")
+    @GetMapping("/index")
     public String hello(@RequestParam(name = "name") String name,Model model){
         model.addAttribute("name",name);
-        return "hello";
+        return "index";
     }
 }
