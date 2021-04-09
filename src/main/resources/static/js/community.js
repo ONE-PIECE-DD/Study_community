87a8b52204f13043c3c90fa659f24e13eede29ee
ONE-PIECE-DD <1510147411@qq.com>
2021-04-09 21:54:56 +0800

    Secondary comments feature front-end page rendering

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -1,4 +1,5 @@
 //该函数在点击回复的时候被调用，获取questionId的值和内容，再在ajax里面设置传递方法、url同时拼接json
+/*提交回复*/
 function post() {
     var questionId = $("#question_id").val();
     var content = $("#comment_content").val();
@@ -26,7 +27,32 @@ function post() {
                     alert(response.message);//浏览器弹窗显示
                 }
             }
+
         },
         dataType:"json"
     });
+}
+/*展开二级评论*/
+function collapseComments(e) {
+
+
+    var id =e.getAttribute("data-id");
+    var comments = $("#comment-"+id);
+    //获取一下二级评论的展开状态
+    var collapse = e.getAttribute("data-collapse");
+    if(collapse){
+        //折叠二级评论
+        comments.removeClass("in");
+        e.removeAttribute("data-collapse");
+        e.classList.remove("active");
+    }else {
+        //展开二级评论
+        comments.addClass("in");
+        //标记二级评论展开状态
+        e.setAttribute("data-collapse","in");
+        e.classList.add("active");
+    }
+
+
+
 }
