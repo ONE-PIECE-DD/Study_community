1f951035a6d394585d7875ba33d40b5e13ca72e7
ONE-PIECE-DD <1510147411@qq.com>
2021-05-14 14:55:11 +0800

    add the function about loading the local images

--- src/main/java/com/onepiece/community/community/controller/FileController.java
+++ src/main/java/com/onepiece/community/community/controller/FileController.java
@@ -1,4 +1,19 @@
 package com.onepiece.community.community.controller;
 
+import com.onepiece.community.community.dto.FileDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class FileController {
+    @RequestMapping("/file/upload")
+    @ResponseBody
+    public FileDTO upload(){
+        FileDTO fileDTO = new FileDTO();
+        fileDTO.setSuccess(1);
+        fileDTO.setUrl("/images/mylove.jpg");
+        return fileDTO;
+    }
 }
--- src/main/java/com/onepiece/community/community/dto/FileDTO.java
+++ src/main/java/com/onepiece/community/community/dto/FileDTO.java
@@ -1,4 +1,10 @@
 package com.onepiece.community.community.dto;
 
+import lombok.Data;
+
+@Data
 public class FileDTO {
+    private int success;
+    private String massage;
+    private String url;
 }
